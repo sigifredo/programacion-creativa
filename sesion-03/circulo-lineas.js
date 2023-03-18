707b5bc9810af61546d33a755a069f5da08cc1d3
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2023-03-18 12:24:11 -0500

    circulo-lineas.js added.

---
+++ sesion-03/circulo-lineas.js
@@ -0,0 +1,26 @@
+const SIDE = 500;
+
+function setup() {
+    createCanvas(SIDE, SIDE);
+
+    angleMode(DEGREES);
+}
+
+function draw() {
+    let colors = ['rgba(255,0,0,0.3)', 'rgba(0,0,255,0.3)', 'rgba(255,192,203,0.3)', 'rgba(0,128,0,0.3)', 'rgba(128,0,128,0.3)'];
+
+    const RADIO = 200;
+    translate(SIDE / 2, SIDE / 2);
+
+    let angle1 = random(0, 360);
+    let x1 = sin(angle1) * RADIO;
+    let y1 = cos(angle1) * RADIO;
+
+    let angle2 = random(0, 360);
+    let x2 = sin(angle2) * RADIO;
+    let y2 = cos(angle2) * RADIO;
+
+    strokeWeight(random(1, 5));
+    stroke(random(colors));
+    line(x1, y1, x2, y2);
+}
